fae317db437c9d69459d3e8b1e15fef31e29e961
Kai Wells <support@kaiwells.me>
2026-07-14 22:43:59 -0700

    chore: scaffold cobra root command

--- cmd/patvault/main.go
+++ cmd/patvault/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	fmt.Println("patvault")
+	root := &cobra.Command{
+		Use:   "patvault",
+		Short: "GitHub PAT credential helper with encrypted storage",
+	}
+	if err := root.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
